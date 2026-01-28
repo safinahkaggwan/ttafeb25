642aa0cfe1430e48988f3c6458ff2cefcd8d8593
Safinah Kaggwa <89240575+safinahkaggwan@users.noreply.github.com>
2026-01-28 10:37:39 +0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('db_aba60f_ttasystem25', 'db_aba60f_ttasystem25_admin', '2025TTA#system', {
-    host: 'SQL1003.site4now.net',
+const sequelize = new Sequelize('db_ac3a32_tanzaniatta001', 'db_ac3a32_tanzaniatta001_admin', 'TTAtz2026!!', {
+    host: 'SQL8010.site4now.net',
     dialect: 'mssql',
     dialectOptions: {
         options: {
