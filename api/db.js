6814603c6716a9cfeba7cdd7acb17db12b151c45
Safinah Kaggwa <89240575+safinahkaggwan@users.noreply.github.com>
2026-02-03 00:38:19 +0300

    Update DB password in api/db.js to match provider connection string

--- api/db.js
+++ api/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('db_ac3a32_tanzaniatta001', 'db_ac3a32_tanzaniatta001_admin', 'TTAtz2026!!', {
+const sequelize = new Sequelize('db_ac3a32_tanzaniatta001', 'db_ac3a32_tanzaniatta001_admin', 'TTAtz2026', {
     host: 'SQL8010.site4now.net',
     dialect: 'mssql',
     dialectOptions: {
@@ -22,4 +22,3 @@ sequelize.authenticate()
 });
 
 module.exports = sequelize;
-
